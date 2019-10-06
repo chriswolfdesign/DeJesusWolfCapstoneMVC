ba6b828ee5d656045ef9fa2b7f7e8f567ed2db65
chriswolfdesign <chriswolfdesign@gmail.com>
2019-10-05 20:39:59 -0400

    Created taskCards

---
+++ src/model/task_card.js
@@ -0,0 +1,26 @@
+/**
+ * task_card.js
+ *
+ * The JavaScript class that represents a Task Card in our Agile Development
+ * Board.
+ *
+ * @author Ellery De Jesus
+ * @author Chris Wolf
+ * @version 2.0.0 (October 5, 2019)
+ */
+
+class TaskCard {
+  /**
+   * Generates the TaskCard object
+   *
+   * @param {string} label the label representing this task card
+   * @param {string} text the text this task card should display
+   */
+  constructor(label, text) {
+    this.label = label;
+    this.text = text;
+  } // end constructor
+} // end class
+
+// export the class
+module.exports.TaskCard = TaskCard;
