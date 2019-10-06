6ed2ab2809bda01564a8e61b9b4b605b709effef
chriswolfdesign <chriswolfdesign@gmail.com>
2019-10-05 22:17:22 -0400

    Created failing tests for ListFactory

---
+++ src/model/factories/list_factory.js
@@ -0,0 +1,29 @@
+/**
+ * list_factory.js
+ *
+ * The JavaScript class that will allow us to easily autogenerate template
+ * lists for our Agile Development Board.
+ *
+ * @author Ellery De Jesus
+ * @author Chris Wolf
+ * @version 2.0.0 (October 5, 2019)
+ */
+
+class ListFactory {
+  // Constructor delibrately not used
+
+  /**
+   * generates a list based on the parameter passed in
+   *
+   * @param {ListOption} list the type of list the user wants generated
+   *
+   * @return {List} a list set up based on user preference
+   */
+  generateList(option) {
+    // TODO: Implement
+    return;
+  } // end generateList
+} // end ListFactory
+
+// export this class
+module.exports.ListFactory = ListFactory;
