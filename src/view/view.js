6a0c78a6d61ffbf697996d910c8ee4378c299a91
chriswolfdesign <chriswolfdesign@gmail.com>
2019-10-21 06:33:33 -0400

    Created a template for view

---
+++ src/view/view.js
@@ -0,0 +1,26 @@
+/**
+ * view.js
+ *
+ * The class responsible for generating the HTML based off our current model
+ *
+ * @author Chris Wolf
+ * @version 2.0.0 (October 21, 2019)
+ */
+
+class View {
+  // Intentionally no constructor
+
+  /**
+   * generates HTML based on the current model
+   *
+   * @param {Model} the model we are generating HTML for
+   *
+   * @return {string} the HTML for model
+   */
+  generateHTML(model) {
+    return '<h1>TODO: Implement</h1>'
+  } // end generateHTML
+} // end View
+
+// export this class
+module.exports.View = View;
