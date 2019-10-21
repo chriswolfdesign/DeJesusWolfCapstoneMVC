4324cdbb63ec4df47152fdb53632939758da71fb
chriswolfdesign <chriswolfdesign@gmail.com>
2019-10-21 19:38:59 -0400

    Updated style to be the same among all files

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -3,76 +3,124 @@ let View = require('../view/view.js').View;
 let BoardOptions = require('../model/enums/board_options.js').BoardOptions;
 
 class Controller{
-    constructor() {
-        this.model = new App('MoSCoW Board');
-        this.model.setController(this);
-        this.view = new View();
-        this.generateBoardTemplate(BoardOptions.MOSCOW);
-
-        /**
-         * DEMO CODE
-         * Simply for demonstration purposes
-         * Remove after sprint review
-         */
-        this.model.boards[0].lists[0].addTask('MT1', 'Must Task 1');
-        this.model.boards[0].lists[0].addTask('MT2', 'Must Task 2');
-
-        this.model.boards[0].lists[1].addTask('CT1', 'Could Task 1');
-        this.model.boards[0].lists[1].addTask('CT2', 'Could Task 2');
-
-        this.model.boards[0].lists[2].addTask('ST1', 'Should Task 1');
-        this.model.boards[0].lists[2].addTask('ST2', 'Should Task 2');
-
-        this.model.boards[0].lists[3].addTask('WT1', 'Wont Task 1');
-        this.model.boards[0].lists[3].addTask('WT2', 'Wont Task 2');
-        /**
-         * END DEMO CODE
-         */
-
-    }
-
-    //generateBoard(label, color);
-
-    generateBoardTemplate(option) {
-        this.model.generateBoardTemplate(option);
-    }
-
-    removeBoard(boardID) {
-        this.model.removeBoard(boardID);
-    }
-
-    generateList(boardID, label, color) {
-        this.model.generateList(boardID, label, color);
-    }
-
-    removeList(boardID, listID) {
-        this.model.removeList(boardID, listID);
-    }
-
-    generateListTemplate(boardID, option) {
-        this.model.generateListTemplate(boardID, option);
-    }
-
-    generateTaskCard(boardID, listID, label, text) {
-        this.model.generateTaskCard(boardID, listID, label, text);
-    }
-
-    removeTaskCard(boardID, listID, cardID) {
-        this.model.removeTaskCard(boardID, listID, cardID);
-    }
-
-    getModel() {
-        return this.model;
-    }
+  constructor() {
+    this.model = new App('MoSCoW Board');
+    this.model.setController(this);
+    this.view = new View();
+    this.generateBoardTemplate(BoardOptions.MOSCOW);
 
     /**
-     * Calls the view class to generate HTML based on the given model
-     *
-     * @return {String} html based off of model
-     */
-    generateHTML() {
-        return this.view.generateHTML(this.model);
-    }
-}
+    * DEMO CODE
+    * Simply for demonstration purposes
+    * Remove after sprint review
+    */
+    this.model.boards[0].lists[0].addTask('MT1', 'Must Task 1');
+    this.model.boards[0].lists[0].addTask('MT2', 'Must Task 2');
 
+    this.model.boards[0].lists[1].addTask('CT1', 'Could Task 1');
+    this.model.boards[0].lists[1].addTask('CT2', 'Could Task 2');
+
+    this.model.boards[0].lists[2].addTask('ST1', 'Should Task 1');
+    this.model.boards[0].lists[2].addTask('ST2', 'Should Task 2');
+
+    this.model.boards[0].lists[3].addTask('WT1', 'Wont Task 1');
+    this.model.boards[0].lists[3].addTask('WT2', 'Wont Task 2');
+    /**
+    * END DEMO CODE
+    */
+
+  } // end constructor
+
+  /**
+   * calls on the model to create a new board from a template
+   *
+   * @param {BoardOption} the template we are using to build a new board
+   */
+  generateBoardTemplate(option) {
+    this.model.generateBoardTemplate(option);
+  } // end generateBoardTemplate
+
+  /**
+   * removes a board from our model
+   *
+   * @param {string} boardID the id for the board we are removing
+   */
+  removeBoard(boardID) {
+    this.model.removeBoard(boardID);
+  } // end removeBoard
+
+  /**
+   * generates a list with the specific credentials
+   *
+   * @param {string} boardID the board to add the list to
+   * @param {string} label the label for the new list
+   * @param {Colors} the color of the new list
+   */
+  generateList(boardID, label, color) {
+    this.model.generateList(boardID, label, color);
+  } // end generateList
+
+  /**
+   * removes a list from a board
+   *
+   * @param {string} boardID the board we are removing a list from
+   * @param {string} listID the list we are removing
+   */
+  removeList(boardID, listID) {
+    this.model.removeList(boardID, listID);
+  } // end removeList
+
+  /**
+   * generates a list from a template
+   *
+   * @param {string} boardID the board we are adding a list to
+   * @param {ListOption} option the template we would like to build a list from
+   */
+  generateListTemplate(boardID, option) {
+    this.model.generateListTemplate(boardID, option);
+  } // end generateListTemplate
+
+  /**
+   * generates a task card with the given credentials
+   *
+   * @param {string} boardID the board we are adding a task card to
+   * @param {string} listID the list we are adding a task card to
+   * @param {string} label the label for the new task card
+   * @param {string} text the text for the new task card
+   */
+  generateTaskCard(boardID, listID, label, text) {
+    this.model.generateTaskCard(boardID, listID, label, text);
+  } // end generateTaskCard
+
+  /**
+   * removes a task card from a list
+   *
+   * @param {string} boardID the board from which we are removing a task card
+   * @param {string} listID the list from which we are removing a task card
+   * @param {string} cardID the task we are removing
+   */
+  removeTaskCard(boardID, listID, cardID) {
+    this.model.removeTaskCard(boardID, listID, cardID);
+  } // end removeTaskCard
+
+  /**
+   * getter for model
+   *
+   * @return {App} the model this controller controls
+   */
+  getModel() {
+    return this.model;
+  } // end getModel
+
+  /**
+   * Calls the view class to generate HTML based on the given model
+   *
+   * @return {String} html based off of model
+   */
+  generateHTML() {
+    return this.view.generateHTML(this.model);
+  } // end generateHTML
+} // end Controller
+
+// export this class
 module.exports.Controller = Controller
