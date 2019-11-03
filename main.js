a27ba0850561008c0765b062ce1339a3a9486e22
chriswolfdesign <chriswolfdesign@gmail.com>
2019-11-03 06:44:04 -0500

    Created template for index.html and main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+/**
+ * main.js
+ *
+ * Acts as the HTML injector for our web application
+ *
+ * @author Ellery De Jesus
+ * @author Chris Wolf
+ * @version 2.0.0 (November 3, 2019)
+ */
+
+window.onload = function() {
+  alert('Main.js is running!');
+} // end window.onload
